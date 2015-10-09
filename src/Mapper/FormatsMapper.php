73c19708b800e702a9964a4dcc4b141ac072e9bc
Darius Smaliukas <dariussm@gmail.com>
2015-10-09 14:46:20 +0300

    fix

--- src/Mapper/FormatsMapper.php
+++ src/Mapper/FormatsMapper.php
@@ -11,14 +11,14 @@ class FormatsMapper implements MapperInterface
         $formats = [];
 
         foreach ($data as $format) {
-            if (in_array($format, array('protocol', 'segment_urls', 'initialization_url'))) {
-                continue;
-            }
-            
             $entity = new Format();
             $reflection = new \ReflectionObject($entity);
 
             foreach ($format as $field => $value) {
+                if (in_array($field, array('protocol', 'segment_urls', 'initialization_url'))) {
+                    continue;
+                }
+                
                 $prop = $reflection->getProperty(lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', $field)))));
                 $prop->setAccessible(true);
                 $prop->setValue($entity, $value);
